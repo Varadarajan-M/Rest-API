b5e8cf4c8c006ae837bf9b237b3ab2348f99cd6e
hemanth.ramadugu <hemanth.ramadugu@pushpak.ai>
2021-08-12 10:17:05 +0530

    Added model structure

--- db/books.model.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const { schemaOptions } = require('./options');
-
-const BookSchema = new mongoose.Schema(schemaOptions);
-const Book = mongoose.model('Book', BookSchema, 'Books');
-
-module.exports = Book;
